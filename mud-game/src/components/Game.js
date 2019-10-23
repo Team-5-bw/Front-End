d85c78f19c0dd8ecc69368c2ccb882a28346a1fc
Agustina Sosa <agustinasf85@gmail.com>
2019-10-23 18:09:01 -0400

    Added react-reveal dependency and added handleMove() and logout()

--- mud-game/src/components/Game.js
+++ mud-game/src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Fade from 'react-reveal';
 
 class Game extends Component {
     constructor(props) {
@@ -12,7 +13,8 @@ class Game extends Component {
           roomDescription: "",
           roomPlayers: "",
           username: "",
-          uuid: ""
+          uuid: "",
+          direction: ""
         };
     }
     
@@ -20,12 +22,23 @@ class Game extends Component {
         this.init()
     };
 
+
+    logout = () => {
+        this.clickE();
+        this.ejectE();
+
+        setTimeout(() =>{
+            localStorage.removeItem('token');
+            window.location.assign('/');
+        }, 3000);
+    };
+
     init = () => {
         const herokurl = 'https://team5-mud.herokuapp.com';
         const key = localStorage.getItem('token');
 
         axios({
-            url: `${herokurl}/api/adv/rooms`,
+            url: `${herokurl}/api/adv/init`,  // confirm this is the right path
             method: 'GET',
             headers: {
                 Authentication: `${key}`
@@ -45,6 +58,49 @@ class Game extends Component {
         .catch(err => {
             console.log('init catch: ', err.response)
         });
+    };
+
+    handleMove = direction => {
+        const herokurl: 'https://team5-mud.herokuapp.com';  // whats wrong here???
+
+        axios({
+            url: `${herokurl}/api/adv/move`, // confirm this is the correct path
+            method: 'POST',
+            headers: {
+                Authentication: `${this.state.token}`
+            },
+            data: {
+                direction: direction 
+            }  
+        })
+        .then(res => {
+            this.setState({
+                roomTitle: res.data.title,
+                roomDescription: res.data.description,
+                roomPlayers: res.data.players,
+                errorMsg: res.data.error_msg,
+                movePlayer: true
+            })
+        })
+        .catch(err => {
+            console.log('handleMove catch: ', err.response)
+        })
+    };
+
+    clickE = () => {
+        document.getElementById('click');
+    };
+
+    moveE = () => {
+        document.getElementById('move');
+    };
+
+    ejectE = () => {
+        document.getElementById('eject');
+    }
+    
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
     };
 };
 
