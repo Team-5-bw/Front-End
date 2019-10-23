6325142fb7837c92abf1f8bbd30ef0ebcdac6c00
Agustina Sosa <agustinasf85@gmail.com>
2019-10-23 16:54:30 -0400

    created init() in Game.js

--- mud-game/src/components/Game.js
+++ mud-game/src/components/Game.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-class Game {
-    return 
-}
+class Game extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          token: "",
+          playerName: "",
+          roomTitle: "",
+          roomDescription: "",
+          roomPlayers: "",
+          username: "",
+          uuid: ""
+        };
+    }
+    
+    componentDidMount() {
+        this.init()
+    };
+
+    init = () => {
+        const herokurl = 'https://team5-mud.herokuapp.com';
+        const key = localStorage.getItem('token');
+
+        axios({
+            url: `${herokurl}/api/adv/rooms`,
+            method: 'GET',
+            headers: {
+                Authentication: `${key}`
+            }
+        })
+        .then(res => {
+            console.log('init: ', res.data);
+            this.setState({
+                playerName: res.data.name,
+                roomTitle: res.data.title,
+                roomDescription: res.data.description,
+                roomPlayers: res.data.players,
+                uuid: res.data.uuid,
+                token: key
+            });
+        })
+        .catch(err => {
+            console.log('init catch: ', err.response)
+        });
+    };
+};
 
 export default Game;
