0b88e2f535ab0baf88578ec88896f6d636cf9415
ken1286 <kindle1286@gmail.com>
2019-10-22 22:11:37 -0700

    Register and login components done, no styling yet

--- mud-game/src/App.js
+++ mud-game/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Game from './components/Game.js';
+import Register from './components/Register.js';
+import Login from './components/Login.js';
 // import Register from './components/Register.js';
 // import Login from './components/Login.js';
 
 import './App.css';
 
-
 class App extends Component {
   render() {
     return (
       <Switch>
-      {/* <Route path='/register' component= {Register} />
-        <Route path='/login' component= {Login} /> */}
-        <Route /*ProtectedRoute*/ path='/' component= {Game} />
+        {/* { <Route path='/register' component= {Register} /> */}
+        <Route path='/login' component={Login} />}
+        {/* <Route /*ProtectedRoute* path='/' component= {Game} /> */}
+        <Route /*ProtectedRoute*/ path='/register' component={Register} />
       </Switch>
-    )
+    );
   }
 }
 
--- mud-game/src/components/Register.js
+++ mud-game/src/components/Register.js
@@ -1,46 +1,89 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
-    state = {
-      username: "",
-      email: "",
-      password: ""
-    };
+  state = {
+    username: '',
+    email: '',
+    password1: '',
+    password2: ''
+  };
 
-    inputChangeHandler = e => {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    };
+  inputChangeHandler = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    submitValues = ({ username, email, password }) => {
-        console.log({ username, email, password });
-        this.setState({
-          username: username,
-          email: email,
-          password1: password,
-          
-        });
-        const herokurl = "https://lambda-mud-test.herokuapp.com/";
-        console.log("this state", this.state);
-    
-        axios({
-          url: `${herokurl}/api/registration/`,
-          method: "POST",
-          data: {
-            username: `${this.state.username}`,
-            password: `${this.state.password1}`,
-            email: `${this.state.email}`
-          }
-        })
-        .then(res => {
-            console.log("response", res);
-            const token = res.data["key"];
-            localStorage.setItem("token", `Token ${token}`);
-            this.props.history.push("/adventure");
-        })
-        .catch(err => {
-            console.log('Axios error:', err)
-        });
+  submitValues = ({ username, email, password1, password2 }) => {
+    console.log({ username, email, password1, password2 });
+    this.setState({
+      username: username,
+      email: email,
+      password1: password1,
+      password2: password2
+    });
+    const herokurl = 'https://lambda-mud-test.herokuapp.com';
+    console.log('this state', this.state);
 
-};
+    axios({
+      url: `${herokurl}/api/registration/`,
+      method: 'POST',
+      data: {
+        username: `${this.state.username}`,
+        password1: `${this.state.password1}`,
+        password2: `${this.state.password2}`,
+        email: `${this.state.email}`
+      }
+    })
+      .then(res => {
+        console.log('response', res);
+        const token = res.data['key'];
+        localStorage.setItem('token', `Token ${token}`);
+        this.props.history.push('/adventure');
+      })
+      .catch(err => {
+        console.log('Axios error:', err);
+      });
+  };
+
+  submitHandler = ev => {
+    ev.preventDefault();
+    this.submitValues(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={e => this.submitHandler(e)}>
+        <input
+          type='text'
+          name='username'
+          value={this.state.username}
+          onChange={this.inputChangeHandler}
+        ></input>
+        <input
+          type='email'
+          name='email'
+          value={this.state.email}
+          onChange={this.inputChangeHandler}
+        ></input>
+        <input
+          type='password'
+          name='password1'
+          value={this.state.password1}
+          onChange={this.inputChangeHandler}
+        ></input>
+        <input
+          type='password'
+          name='password2'
+          value={this.state.password2}
+          onChange={this.inputChangeHandler}
+        ></input>
+        <button type='submit' onSubmit={e => this.submitHandler(e)}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Register;
